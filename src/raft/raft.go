db80cd34f37b1d53287593409b61da53b4047fd6
Zixuan <dyxdy@live.com>
2018-02-17 16:50:10 +0800

    Finish lab2. Test passed.

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -1,6 +1,8 @@
 package raft
 
 import (
+	"bytes"
+	"labgob"
 	"labrpc"
 	"math/rand"
 	"sync"
@@ -88,42 +90,25 @@ func (rf *Raft) GetState() (int, bool) {
 	return term, isleader
 }
 
-//
-// save Raft's persistent state to stable storage,
-// where it can later be retrieved after a crash and restart.
-// see paper's Figure 2 for a description of what should be persistent.
-//
 func (rf *Raft) persist() {
-	// Your code here (2C).
-	// Example:
-	// w := new(bytes.Buffer)
-	// e := labgob.NewEncoder(w)
-	// e.Encode(rf.xxx)
-	// e.Encode(rf.yyy)
-	// data := w.Bytes()
-	// rf.persister.SaveRaftState(data)
+	buf := new(bytes.Buffer)
+	encoder := labgob.NewEncoder(buf)
+	encoder.Encode(rf.currentTerm)
+	encoder.Encode(rf.votedFor)
+	encoder.Encode(rf.logs)
+	data := buf.Bytes()
+	rf.persister.SaveRaftState(data)
 }
 
-//
-// restore previously persisted state.
-//
 func (rf *Raft) readPersist(data []byte) {
-	if data == nil || len(data) < 1 { // bootstrap without any state?
+	if data == nil || len(data) < 1 {
 		return
 	}
-	// Your code here (2C).
-	// Example:
-	// r := bytes.NewBuffer(data)
-	// d := labgob.NewDecoder(r)
-	// var xxx
-	// var yyy
-	// if d.Decode(&xxx) != nil ||
-	//    d.Decode(&yyy) != nil {
-	//   error...
-	// } else {
-	//   rf.xxx = xxx
-	//   rf.yyy = yyy
-	// }
+	buf := bytes.NewBuffer(data)
+	decoder := labgob.NewDecoder(buf)
+	decoder.Decode(&rf.currentTerm)
+	decoder.Decode(&rf.votedFor)
+	decoder.Decode(&rf.logs)
 }
 
 func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
@@ -149,6 +134,7 @@ func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 	if args.Term == rf.currentTerm {
 		if flag && rf.votedFor == -1 {
 			rf.votedFor = args.Candidate
+			rf.persist()
 		}
 		reply.Term = rf.currentTerm
 		reply.VoteGranted = (rf.votedFor == args.Candidate)
@@ -161,6 +147,7 @@ func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 		rf.votedFor = -1
 		if flag {
 			rf.votedFor = args.Candidate
+			rf.persist()
 		}
 		rf.resetTimer()
 		reply.Term = rf.currentTerm
@@ -225,6 +212,7 @@ func (rf *Raft) handleTimer() {
 		rf.currentTerm += 1
 		rf.votedFor = rf.me
 		rf.vote = 1
+		rf.persist()
 		args := RequestVoteArgs{
 			Term:         rf.currentTerm,
 			Candidate:    rf.me,
@@ -293,6 +281,7 @@ func (rf *Raft) AppendEntry(args *AppendEntryArgs, reply *AppendEntryReply) {
 			reply.Success = true
 		}
 	}
+	rf.persist()
 	rf.resetTimer()
 }
 
@@ -391,6 +380,7 @@ func (rf *Raft) Start(command interface{}) (int, int, bool) {
 		index = len(rf.logs)
 		term = rf.currentTerm
 	}
+	rf.persist()
 	return index, term, isLeader
 }
 
